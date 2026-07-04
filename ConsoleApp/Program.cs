0897230e5fdc5f726b72c55744d58fcfd385e0e0
ecstatiQstudio <info@ecstatiQ.studio>
2026-07-04 04:50:11 -0400

    Code as of end of episode

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Interaction;
 
 Console.WriteLine("Hello World");
 
+string pathToInputMidiFile = Console.ReadLine();
+
+ReadingSettings readingSettings = new ReadingSettings
+{
+    InvalidChannelEventParameterValuePolicy = InvalidChannelEventParameterValuePolicy.SnapToLimits
+};
+
+MidiFile midiFile = MidiFile.Read(pathToInputMidiFile, readingSettings);
+
+int trackNumber = 0;
+List<Classes.Note> notes = new List<Classes.Note>();
+
+foreach (TrackChunk trackChunk in midiFile.GetTrackChunks())
+{
+    trackNumber++;
+
+    foreach (Note note in trackChunk.GetNotes())
+    {
+        notes.Add(new Classes.Note
+        {
+            TrackNumber = trackNumber,
+            NoteName = note.NoteName.ToString(),
+            Time = note.Time,
+            Length = note.Length
+        });
+    }
+}
