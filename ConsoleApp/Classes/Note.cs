bd04d474c9409e7928547878b8ee3f616304c646
ecstatiQstudio <info@ecstatiQ.studio>
2026-07-04 04:50:11 -0400

    Code as of end of episode

---
+++ ConsoleApp/Classes/Note.cs
@@ -0,0 +1,10 @@
+namespace Classes
+{
+    public class Note
+    {
+        public int TrackNumber { get; set; }
+        public string NoteName { get; set; }
+        public float Time { get; set; }
+        public float Length { get; set; }
+    }
+}
