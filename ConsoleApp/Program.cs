66dbd0f62cfd343a13969fcf32740d390cf4d5f1
ecstatiQstudio <info@ecstatiQ.studio>
2026-07-05 22:08:06 -0400

    Code as of end of episode

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 using Melanchall.DryWetMidi.Core;
 using Melanchall.DryWetMidi.Interaction;
 
-Console.WriteLine("Hello World");
+Console.WriteLine("Enter path to configuration file (.json):");
+string pathToConfigurationFile = Console.ReadLine();
+Classes.Configuration configuration = null;
 
-string pathToInputMidiFile = Console.ReadLine();
+using (StreamReader streamReader = new StreamReader(pathToConfigurationFile))
+{
+    string json = streamReader.ReadToEnd();
+
+    configuration = JsonSerializer.Deserialize<Classes.Configuration>(json);
+}
 
 ReadingSettings readingSettings = new ReadingSettings
 {
     InvalidChannelEventParameterValuePolicy = InvalidChannelEventParameterValuePolicy.SnapToLimits
 };
-
-MidiFile midiFile = MidiFile.Read(pathToInputMidiFile, readingSettings);
-
+MidiFile midiFile = MidiFile.Read(configuration.pathToInputMidiFile, readingSettings);
 int trackNumber = 0;
 List<Classes.Note> notes = new List<Classes.Note>();
 
@@ -31,4 +38,13 @@ foreach (TrackChunk trackChunk in midiFile.GetTrackChunks())
             Length = note.Length
         });
     }
+}
+
+Classes.Lyrics lyrics = null;
+
+using (StreamReader streamReader = new StreamReader(configuration.pathToInputTimedLyricsFile))
+{
+    string json = streamReader.ReadToEnd();
+
+    lyrics = JsonSerializer.Deserialize<Classes.Lyrics>(json);
 }
